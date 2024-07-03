a06beffa25388f4bb4207776ae98e9fce799ebd8
ANIRUDH SHARMA <129547564+ANIRUDH130903@users.noreply.github.com>
2024-07-03 14:33:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from langchain_huggingface import HuggingFaceEndpoint
+st.title("LLM MODEL")
+repo_id="mistralai/Mistral-7B-Instruct-v0.2"
+llm=HuggingFaceEndpoint(repo_id=repo_id,max_length=128,temperature=0.7)
+ip = st.text_input("Enter your question")
+btn = st.button("Ask")
+if btn:
+  st.write(llm.invoke(ip))
