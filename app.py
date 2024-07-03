333e270b0d96e23f322a951d0f4a38cd98a0227d
ANIRUDH SHARMA <129547564+ANIRUDH130903@users.noreply.github.com>
2024-07-03 17:40:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from langchain_huggingface import HuggingFaceEndpoint
 st.title("LLM MODEL")
-repo_id="mistralai/Mistral-7B-Instruct-v0.3"
-llm=HuggingFaceEndpoint(repo_id=repo_id,max_length=128,temperature=0.7)
+repo_id="mistralai/Mistral-7B-Instruct-v0.2"
+llm=HuggingFaceEndpoint(repo_id=repo_id,max_length=120,temperature=0.7)
 ip = st.text_input("Enter your question")
 btn = st.button("Ask")
 if btn:
